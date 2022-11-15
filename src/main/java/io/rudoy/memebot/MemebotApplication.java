d125ca0816ce6d3ac94ce94a68069455ccb5c17b
rudoy <rudoy@nca.by>
2022-11-15 16:46:05 +0300

    first commit develop

---
+++ src/main/java/io/rudoy/memebot/MemebotApplication.java
@@ -0,0 +1,13 @@
+package io.rudoy.memebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemeBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemeBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/rudoy/memebot/MemebotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.rudoy.memebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemebotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
